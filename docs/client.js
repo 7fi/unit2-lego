2453454c464e216a01a21636d33210e8fc484120
7fi <carterwilliamanderson@gmail.com>
2022-06-06 17:22:34 -0700

    added different parts and pics and new steps

--- docs/client.js
+++ docs/client.js
@@ -6,28 +6,70 @@ const stepImg = document.getElementById("stepImg");
 const nextStep = document.getElementById("nextStep");
 const prevStep = document.getElementById("prevStep");
 
+const needleTab = document.getElementById('needleTab');
+const ferryTab = document.getElementById('ferryTab');
+const mountainTab = document.getElementById('mountainTab');
+const baseTab = document.getElementById('baseTab');
+
 const modeToggle = document.getElementById("modeToggle");
 let lightMode = true;
 
-const steps = [
-    {instruction:"Stack two black four prong Legos", img:"../img/step1.png"},
-    {instruction:"Stack one yellow four prong Lego directly on top of the stack created in previous step."},
-    {instruction:"Stack two yellow two prong Lego directly on top of stack created in previous step."},
-    {instruction:"Stack two sage green two prong Lego directly on top of stack created in previous step.",img:"../img/step4.png"},
-    {instruction:"Repeat Step 4, but stack Lego perpendicularly this time.",img:"../img/step5.png"},
-    {instruction:"Stack two grey four prong Lego directly on top of stack created in previous step.",img:"../img/step6.png"},
-    {instruction:"Stack two flat black four prong Lego directly on top of stack created in previous step.",img:"../img/step7.png"},
-    {instruction:"Stack one flat tan eight prong Lego on top of stack created in previous step, centered so that two prongs stick out of stack on both ends.",img:"../img/step8.png"},
-    {instruction:"Stack one flat olive green twelve prong Lego on top of stack created in previous step, centered so that two prongs stick out of stack on both ends.",img:"../img/step9.png"},
-    {instruction:"Stack one flat light green sixteen prong Lego on top of stack created in previous step, centered so that two prongs stick out of stack on both ends.",img:"../img/step10.png"},
-    {instruction:"Stack one flat green sixteen prong Lego directly on top of stack created in previous step.",img:"../img/step11.png"},
-    {instruction:"Stack one flat light green sixteen prong Lego on top of stack created in previous step.",img:"../img/step12.png"},
-    {instruction:"Stack one flat olive green twelve prong Lego on top of stack created in previous step, centered so that two prongs from last Lego added stick out on both ends",img:"../img/step13.png"},
-    {instruction:"Stack two flat olive green four prong Lego on top of stack created in previous step, centered so that two prongs from last Lego added stick out on both ends.",img:"../img/step14.png"},
-    {instruction:"Stack one flat tan eight prong Lego directly on top of stack created in previous step",img:"../img/step15.png"},
-    {instruction:"Stack one black circular Lego on top of stack created in previous step, centered so that two prongs from last Lego added stick out on both ends.",img:"../img/step16.png"},
-    {instruction:"Stack two brown connector Lego on top of each other and place them on stack created in previous step.",img:"../img/step17.png"},
-    {instruction:"Stack one transparent yellow cap Lego on top of stack created in previous step.",img:"../img/step18.png"},
+const baseSteps = [
+    {instruction:"Lay the flat 72-prong green Lego horizontally.", img:"../img/base/step1.png"},
+    {instruction:"Place one 8-prong pale blue Lego horizontally in the upper left corner of the base Lego.", img:"../img/base/step2.png"},
+    {instruction:"Place one 12-prong sky blue Lego below pale blue Lego.", img:"../img/base/step3.png"},
+    {instruction:"Put two 4-prong narrow sky blue Legos to the right of the 12-prong Lego.", img:"../img/base/step4.png"},
+    {instruction:"Put two 2-prong sky blue Legos to the right of the narrow Legos.", img:"../img/base/step5.png"},
+    {instruction:"Starting at the bottom left corner of the green base, place three 6-prong dark blue Legos side by side.", img:"../img/base/step6.png"},
+    {instruction:"Place three 2-prong dark blue Legos to the right of the other dark blue Legos.", img:"../img/base/step7.png"},
+    {instruction:"Stack two 2-prong light blue Legos horizontally on top of the dark blue Legos in the bottom right corner of the base.", img:"../img/base/step8.png"},
+    {instruction:"Stack two 8-prong light blue Legos side by side to the left of the two 2-prong light blue Legos.", img:"../img/base/step9.png"},
+];
+const ferrySteps = [
+    {instruction:"Place two 2-prong slanted white Legos on each end of one flat 16-prong green Lego.", img:"../img/ferry/step1.png"},
+    {instruction:"Place two 4-prong narrow white Legos with holes in between slanted white Legos. Make sure the holes are facing outwards.", img:"../img/ferry/step2.png"},
+    {instruction:"Stack two flat 6-prong light blue Legos on top of the white Legos.", img:"../img/ferry/step3.png"},
+    {instruction:"Centered in the middle of the light blue Legos, stack one 4-prong white square Lego.", img:"../img/ferry/step4.png"},
+    {instruction:"Place one 2-prong clear Lego on each side of the white square Lego.", img:"../img/ferry/step5.png"},
+];
+const mountainSteps = [
+    {instruction:"Place two 2-prong grey Legos to the right of one 12-prong grey Lego.", img:"../img/mountain/step1.png"},
+    {instruction:"Stack two 8-prong green Legos directly on top of grey Legos.", img:"../img/mountain/step2.png"},
+    {instruction:"Stack two 2-prong lime green Legos in the center of the green Legos.", img:"../img/mountain/step3.png"},
+    {instruction:"Place one 8-prong green Lego on each side of the lime green Legos.", img:"../img/mountain/step4.png"},
+    {instruction:"Put one 1-prong slanted green Lego underneath the overhanging prongs on the right side.  Note: the slanted side should be facing away from you.", img:"../img/mountain/step5.png"},
+    {instruction:"In the center of the stack, place two 6-prong brown Legos side by side.", img:"../img/mountain/step6.png"},
+    {instruction:"Add one 2-prong red/brown Lego to each side of the brown Legos.", img:"../img/mountain/step7.png"},
+    {instruction:"Stack three 4-prong light brown Legos directly on top of the brown Legos.", img:"../img/mountain/step8.png"},
+    {instruction:"Add one 2-prong transparent yellow Lego to each side of the light brown Legos.", img:"../img/mountain/step9.png"},
+    {instruction:"Stack two 6-prong tan Legos side by side directly on top of the light brown Legos.", img:"../img/mountain/step10.png"},
+    {instruction:"Stack two tall 2-prong white Legos in the center of the tan Legos.", img:"../img/mountain/step11.png"},
+    {instruction:"Add one 2-prong white Lego to each side of the tall white Legos.", img:"../img/mountain/step12.png"},
+    {instruction:"Place one 2-prong clear Lego on both sides of the white Legos.", img:"../img/mountain/step13.png"},
+    {instruction:"Stack one 2-prong clear Lego on top of each short white Lego.", img:"../img/mountain/step14.png"},
+    {instruction:"Stack one 4-prong white Lego on the top in the center of the entire stack.", img:"../img/mountain/step15.png"},
+];
+
+const needleSteps = [
+    {instruction:"Stack two 4-prong black square Legos directly on top of each other.", img:"../img/needle/step1.png"},
+    {instruction:"Add one 4-prong yellow square Lego.", img:"../img/needle/step2.png"},
+    {instruction:"Stack two 2-prong light green/yellow Legos side by side on top of the yellow square.", img:"../img/needle/step3.png"},
+    {instruction:"Add two 2-prong sage green Legos.",img:"../img/needle/step4.png"},
+    {instruction:"Repeat step 4.",img:"../img/needle/step5.png"},
+    {instruction:"Stack two 4-prong light grey Legos on top of the sage green Legos.",img:"../img/needle/step6.png"},
+    {instruction:"Stack two 2-prong light grey Legos side by side on top of the other light grey Legos.",img:"../img/needle/step7.png"},
+    {instruction:"Add two flat 4-prong black square Legos.",img:"../img/needle/step8.png"},
+    {instruction:"Stack one flat 8-prong tan Lego centered on top of the stack.",img:"../img/needle/step9.png"},
+    {instruction:"Add one flat 12-prong lime green Lego centered on top of the tan Lego.",img:"../img/needle/step10.png"},
+    {instruction:"Add one flat 16-prong light green/yellow Lego centered on top of the lime green Lego.",img:"../img/needle/step11.png"},
+    {instruction:"Stack one flat 16-prong green Lego directly on top of the light green/yellow Lego.",img:"../img/needle/step12.png"},
+    {instruction:"Stack one flat 16-prong light green/yellow Lego directly on top of the green Lego.",img:"../img/needle/step13.png"},
+    {instruction:"Add one flat 12-prong lime green Lego in the center of the light green/yellow Lego.",img:"../img/needle/step14.png"},
+    {instruction:"Add two flat 4-prong lime green square Legos side by side in the center of the lime green Lego.",img:"../img/needle/step15.png"},
+    {instruction:"Stack one flat 8-prong tan Lego directly on top of the two lime green Legos.",img:"../img/needle/step16.png"},
+    {instruction:"Stack one black round Lego in the center of the tan Lego.",img:"../img/needle/step17.png"},
+    {instruction:"In the center of the black Lego, stack two brown connector Legos on top of each other.",img:"../img/needle/step18.png"},
+    {instruction:"Add clear yellow cap Lego to the top of the connectors.",img:"../img/needle/step19.png"},
 ]
 
 function load(){
--- docs/instructions.js
+++ docs/instructions.js
@@ -1,33 +1,166 @@
-loadStep(1);
+let currentTab = "base";
 
-for (let i = 0; i < steps.length; i++) {
-    const stepSelector = document.createElement('div');
-    stepSelector.innerHTML = i + 1;
-    stepSelector.classList.add('stepNum')
-    stepSelector.addEventListener('click', () => {
-        loadStep(stepSelector.innerHTML);
-        if(document.getElementsByClassName('stepSelected').length > 0)document.getElementsByClassName('stepSelected')[0].classList.remove('stepSelected');
-        stepSelector.classList.add("stepSelected");
-    })
-    // stepSelector.onclick = loadStep(stepSelector.innerHTML);
-    stepHolder.appendChild(stepSelector);
-    const stepSeperator = document.createElement('div');
-    stepSeperator.classList.add("stepSeperator");
-    stepSeperator.innerHTML = "|";
-    stepHolder.appendChild(stepSeperator);
+loadTab(currentTab);
+loadStep(1, currentTab);
+
+function loadTab(tab){
+    while(stepHolder.childElementCount > 0){
+        stepHolder.children[stepHolder.childElementCount - 1].remove();
+    }
+
+    currentTab = tab;
+    let steps;
+    const baseTab = document.createElement('div');
+    baseTab.classList.add("tabName");
+    baseTab.id = "baseTab";
+    baseTab.onclick = function(){loadTab("base")};
+    baseTab.innerHTML = "Part 1";
+    stepHolder.appendChild(baseTab);
+    const baseSeperator = document.createElement('div');
+    baseSeperator.classList.add("stepSeperator");
+    baseSeperator.innerHTML = "|";
+    stepHolder.appendChild(baseSeperator);
+
+    if(tab == "base"){
+        steps = baseSteps;
+        baseTab.classList.add('curTab');
+        baseSeperator.classList.add('curTab');
+        createSteps(steps);
+    }
+
+    const ferryTab = document.createElement('div');
+    ferryTab.classList.add("tabName");
+    ferryTab.id = "ferryTab";
+    ferryTab.onclick = function(){loadTab("ferry")};
+    ferryTab.innerHTML = "Part 2";
+    stepHolder.appendChild(ferryTab);
+    const ferrySeperator = document.createElement('div');
+    ferrySeperator.classList.add("stepSeperator");
+    ferrySeperator.innerHTML = "|";
+    stepHolder.appendChild(ferrySeperator);
+    
+    if(tab == "ferry"){
+        steps = ferrySteps;
+        ferryTab.classList.add('curTab');
+        ferrySeperator.classList.add('curTab');
+        createSteps(steps);
+    }
+
+    const mountainTab = document.createElement('div');
+    mountainTab.classList.add("tabName");
+    mountainTab.id = "mountainTab";
+    mountainTab.onclick = function(){loadTab("mountain")};
+    mountainTab.innerHTML = "Part 3";
+    stepHolder.appendChild(mountainTab);
+    const mountainSeperator = document.createElement('div');
+    mountainSeperator.classList.add("stepSeperator");
+    mountainSeperator.innerHTML = "|";
+    stepHolder.appendChild(mountainSeperator);
+    
+    if(tab == "mountain"){
+        steps = mountainSteps;
+        mountainTab.classList.add('curTab');
+        mountainSeperator.classList.add('curTab');
+        createSteps(steps);
+    }
+
+    const needleTab = document.createElement('div');
+    needleTab.classList.add("tabName");
+    needleTab.id = "needleTab";
+    needleTab.onclick = function(){loadTab("needle")};
+    needleTab.innerHTML = "Part 4";
+    stepHolder.appendChild(needleTab);
+    const needleSeperator = document.createElement('div');
+    needleSeperator.classList.add("stepSeperator");
+    needleSeperator.innerHTML = "|";
+    stepHolder.appendChild(needleSeperator);
+    if(tab == "needle"){
+        steps = needleSteps;
+        needleTab.classList.add('curTab');
+        needleSeperator.classList.add('curTab');
+        createSteps(steps);
+    }
+
+    // if(tab == "needle"){
+    //     steps = needleSteps;
+    //     // for (let i = 0; i < 8; i++) {
+    //     //     stepHolder.children[i].classList.remove("curTab");
+    //     // }
+        
+    //     needleTab.classList.add('curTab');
+    //     stepHolder.children[7].classList.add('curTab');
+    // }else if(tab == "ferry"){
+    //     steps = ferrySteps;
+    //     for (let i = 0; i < 8; i++) {
+    //         stepHolder.children[i].classList.remove("curTab");
+    //     }
+    //     ferryTab.classList.add('curTab');
+    //     stepHolder.children[3].classList.add('curTab');
+    // }else if(tab == "mountain"){
+    //     steps = mountainSteps;
+    //     for (let i = 0; i < 8; i++) {
+    //         stepHolder.children[i].classList.remove("curTab");
+    //     }
+    //     mountainTab.classList.add('curTab');
+    //     stepHolder.children[5].classList.add('curTab');
+    // }else{
+    //     steps = baseSteps;
+    //     for (let i = 0; i < 8; i++) {
+    //         stepHolder.children[i].classList.remove("curTab");
+    //     }
+    //     baseTab.classList.add('curTab');
+    //     stepHolder.children[1].classList.add('curTab');
+    // }
+
+    // oldSteps = document.getElementsByClassName('stepNum');
+    // for (let i = 8; i < stepHolder.children.length; i++) {
+    //     stepHolder.children[i].classList.add('curTab');
+    // }
+    
+    loadStep(1,currentTab);
+    document.getElementsByClassName('stepNum')[0].classList.add('stepSelected');
 }
-document.getElementsByClassName('stepNum')[0].classList.add('stepSelected');
 
-function loadStep(stepNum){
-    stepInstruction.innerHTML = steps[stepNum - 1].instruction;
-    stepImg.src = steps[stepNum - 1].img;
+function createSteps(steps){
+    for (let i = 0; i < steps.length; i++) {
+        const stepSelector = document.createElement('div');
+        stepSelector.innerHTML = i + 1;
+        stepSelector.classList.add('stepNum')
+        stepSelector.addEventListener('click', () => {
+            loadStep(stepSelector.innerHTML);
+            if(document.getElementsByClassName('stepSelected').length > 0)document.getElementsByClassName('stepSelected')[0].classList.remove('stepSelected');
+            stepSelector.classList.add("stepSelected");
+        })
+        // stepSelector.onclick = loadStep(stepSelector.innerHTML);
+        stepHolder.appendChild(stepSelector);
+        const stepSeperator = document.createElement('div');
+        stepSeperator.classList.add("stepSeperator");
+        stepSeperator.innerHTML = "|";
+        stepHolder.appendChild(stepSeperator);
+    }
+}
+
+function loadStep(stepNum,tab){
+    if(tab == "base"){
+        stepInstruction.innerHTML = baseSteps[stepNum - 1].instruction;
+        stepImg.src = baseSteps[stepNum - 1].img;
+    }else if(tab == "ferry"){
+        stepInstruction.innerHTML = ferrySteps[stepNum - 1].instruction;
+        stepImg.src = ferrySteps[stepNum - 1].img;
+    }else if(tab == "mountain"){
+        stepInstruction.innerHTML = mountainSteps[stepNum - 1].instruction;
+        stepImg.src = mountainSteps[stepNum - 1].img;
+    }else{
+        stepInstruction.innerHTML = needleSteps[stepNum - 1].instruction;
+        stepImg.src = needleSteps[stepNum - 1].img;
+    }
     // console.log(steps[stepNum].instruction);
 }
 
 nextStep.addEventListener('click', () =>{
     let currentStep = parseInt(document.getElementsByClassName('stepSelected')[0].innerHTML);
-    if(currentStep < steps.length){
-        loadStep(currentStep + 1);
+    if(currentStep < needleSteps.length){
+        loadStep(currentStep + 1, currentTab);
         document.getElementsByClassName('stepSelected')[0].classList.remove('stepSelected');
         document.getElementsByClassName('stepNum')[currentStep].classList.add("stepSelected");
     }
@@ -35,7 +168,7 @@ nextStep.addEventListener('click', () =>{
 prevStep.addEventListener('click', () =>{
     let currentStep = parseInt(document.getElementsByClassName('stepSelected')[0].innerHTML);
     if(currentStep > 0){
-        loadStep(currentStep - 1);
+        loadStep(currentStep - 1, currentTab);
         document.getElementsByClassName('stepSelected')[0].classList.remove('stepSelected');
         document.getElementsByClassName('stepNum')[currentStep - 2].classList.add("stepSelected");
     }
